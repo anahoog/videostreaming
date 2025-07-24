930ed180f1eb20eecf5b33e47919e8a0eef8a648
anahoog <anahoog@gmail.com>
2025-07-24 18:27:03 -0300

    Remove arquivos desnecessários

--- projeto_rtp_qos_qoe/scripts/capturar_tcpdump.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-PCAP="capturas/rtp_capture.pcap"
-PORTA=4004
-
-mkdir -p capturas
-
-echo "[INFO] Capturando pacotes RTP na porta $PORTA"
-sudo tcpdump -i any udp port $PORTA -w "$PCAP"
